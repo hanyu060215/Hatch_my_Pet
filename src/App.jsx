b0a104c87d0ef9484f8558bfa36527b028009831
Hanyu Liu <liuhanyu2006@gmail.com>
2025-11-15 15:49:05 -0500

    trivia bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import Test from './Test.jsx';
+import './App.css'
 
 function App() {
-    return <div><Test /></div>
+    return (
+        <div className="app-shell blank-shell">
+            <div className="empty-space" aria-hidden="true" />
+
+            <div className="trivia-bar blank-bar">
+                <p className="bar-text">Placeholder trivia bar · test copy only</p>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
