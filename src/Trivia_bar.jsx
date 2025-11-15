ddacf0491acea327bad6db05b58ba65162ba8593
Hanyu Liu <liuhanyu2006@gmail.com>
2025-11-15 16:57:13 -0500

    cleanup

---
+++ src/Trivia_bar.jsx
@@ -0,0 +1,11 @@
+import "./Triva_bar.css";
+
+const TriviaBar = () => {
+  return (
+    <div className="trivia-bar">
+       <p className="bar-text">Placeholder trivia bar · test copy only</p>
+    </div>
+  );
+};
+
+export default TriviaBar;
