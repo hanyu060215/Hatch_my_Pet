a36852c71b1407a68c1d74d0fc0fd50bf4cfb1ad
hpooyadi <hyadi@hamilton.edu>
2025-11-15 19:32:14 -0500

    progressbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Timer from "./Timer.jsx";
+import Progress_bar from "./Progress_bar.jsx"; 
 import "./Trivia_bar.css";
 import "./App.css";
 
@@ -10,20 +11,19 @@ function App() {
     <div className="app-shell">
       <div className="screen-content">
         <div className="hud">
+            <Progress_bar score={90} maxScore={100} />
+            <Timer duration={60} isActive={startTimer} />
+        </div>
+
+            
             <button className="start-button" onClick={() => setStartTimer(true)}>
                 Start
             </button>
-        </div>
-
-        <div className="timer">
-            <Timer duration={60} isActive={startTimer} />
-        </div>
       </div>
 
       <div className="trivia-bar">
         <p className="bar-text">Placeholder trivia bar · test copy only</p>
       </div>
-
     </div>
   );
 }
--- src/Progress_bar.jsx
+++ src/Progress_bar.jsx
@@ -1,28 +1,14 @@
 import React from "react";
 import "./Progress_bar.css";
 
-const ProgressBar = ({ score = 50, maxScore = 100 }) => {
-  const coverPercent = 100 - (score / maxScore) * 100;
+const Progress_bar = ({ score = 0, maxScore = 100 }) => {
+  const percent = Math.min(100, (score / maxScore) * 100);
 
   return (
-    <div className="progress-bar-container">
-      {/* Solid bar color */}
-      <div className="progress-bar-color"></div>
-
-      {/* White cover that shrinks */}
-      <div
-        className="progress-bar-cover"
-        style={{ width: `${coverPercent}%` }}
-      ></div>
-
-      {/* PNG overlay */}
-      <img
-        src="/barframe.png"  
-        alt="Bar frame"
-        className="progress-bar-overlay"
-      />
+    <div className="progress-bar-wrapper">
+      <div className="progress-bar-fill" style={{ width: `${percent}%` }} />
     </div>
   );
 };
 
-export default ProgressBar;
+export default Progress_bar;
